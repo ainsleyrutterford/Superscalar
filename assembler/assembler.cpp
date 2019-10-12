03b5e8f6783afe6d4eb06801240867cf9236c336
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-10-12 19:56:27 +0100

    Create assembler.py

--- assembler/assembler.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-  std::cout << "Avengers assemble\n";
-  return 0;
-}
---
+++ assembler/assembler.py
@@ -0,0 +1,30 @@
+opcodes = { 
+    'nop'  : 0x00 ,  # nop
+
+    'add'  : 0x01 ,  # add  r1 r2 r3 
+    'addi' : 0x02 ,  # addi r1 r2 c
+    'sub'  : 0x03 ,  # sub  r1 r2 r3
+    'subi' : 0x04 ,  # subi r1 r2 c
+    'mul'  : 0x05 ,  # mul  r1 r2 r3
+    'div'  : 0x06 ,  # div  r1 r2 r3
+    'and'  : 0x07 ,  # and  r1 r2 r3
+    'or'   : 0x08 ,  # or   r1 r2 r3
+
+    'lw'   : 0x09 ,  # lw   r1
+    'la'   : 0x0A ,  # la
+    'li'   : 0x0B ,  # li
+    'sw'   : 0x0C ,  # sw
+    'sa'   : 0x0D ,  # sa
+
+    'b'    : 0x0E ,  # b    dst
+    'beq'  : 0x0F ,  # beq  r1 r2 dst
+    'bne'  : 0x10 ,  # bne  r1 r2 dst
+    'bgt'  : 0x11 ,  # bgt  r1 r2 dst
+    'bge'  : 0x12 ,  # bge  r1 r2 dst
+    'blt'  : 0x13 ,  # blt  r1 r2 dst
+    'ble'  : 0x14 ,  # ble  r1 r2 dst
+    'j'    : 0x15 ,  # j    dst
+    'jr'   : 0x16 ,  # jr   r1
+    'jal'  : 0x17    # jal  r1
+}
+
