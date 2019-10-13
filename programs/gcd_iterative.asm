60a0f24daf6956c96a3123f61b907fdf675fc850
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-10-13 11:39:50 +0100

    Update gcd_iterative.asm

--- programs/gcd_iterative.asm
+++ programs/gcd_iterative.asm
@@ -1,12 +1,27 @@
-; int gcd(int a, int b) {
-;   while (a != b) {
-;     if (a > b) {
-;       a = a – b;
-;     } else {
-;       b = b – a;
-;     }
+; int a = 27;
+; int b = 39;
+; while (a != b) {
+;   if (a > b) {
+;     a = a – b;
+;   } else {
+;     b = b – a;
 ;   }
-;   return a;
 ; }
+; return a;
+
+    ; int a = 27
+    addi r1 r1 27
+    ; int b = 39
+    addi r2 r2 39
 
 while:
+    beq r1 r2 while_done
+    ble r1 r2 less_than
+    sub r1 r1 r2
+    j while
+less_than:
+    sub r2 r2 r1
+    j while
+
+while_done:
+    ; r1 contains a = gcd
