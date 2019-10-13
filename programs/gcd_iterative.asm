8e0b87a606cb5e8d7786adb25e2a16af4cab812f
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-10-13 11:44:46 +0100

    Update comments in gcd_iterative.asm

--- programs/gcd_iterative.asm
+++ programs/gcd_iterative.asm
@@ -9,18 +9,16 @@
 ; }
 ; return a;
 
-    ; int a = 27
-    addi r1 r1 27
-    ; int b = 39
-    addi r2 r2 39
+    addi r1 r1 27         ; int a = 27
+    addi r2 r2 39         ; int b = 39
 
 while:
-    beq r1 r2 while_done ; while (a != b)
-    ble r1 r2 less_than  ; if (a > b)
-    sub r1 r1 r2         ; a = a - b
+    beq r1 r2 while_done  ; while (a != b)
+    ble r1 r2 less_than   ; if (a > b)
+    sub r1 r1 r2          ; a = a - b
     j while
-less_than:               ; else
-    sub r2 r2 r1         ; b = b - a
+less_than:                ; else
+    sub r2 r2 r1          ; b = b - a
     j while
 
 while_done:
