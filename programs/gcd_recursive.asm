5e4deef87630ca361a7ad5af4e69f65e376b7ed4
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-10-13 13:15:23 +0100

    Update gcd_recursive.asm

--- programs/gcd_recursive.asm
+++ programs/gcd_recursive.asm
@@ -1,7 +1,15 @@
+; int a = 27;
+; int b = 39;
+;
 ; int gcd(int a, int b) {
 ;   if (a == b) return a;
 ;   if (a > b) return gcd(a-b, b);
 ;   return gcd(a, b-a);
 ; }
 
+    addi r1 r1 27         ; int a = 27
+    addi r2 r2 39         ; int b = 39
+
 gcd:
+    bne r1 r2 not_equal
+    
