fdb61e1cb6ca732fc5586f142078fc4e93a20b8c
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-10-18 13:47:42 +0100

    Add simple processor.py not yet working

---
+++ processor.py
@@ -0,0 +1,30 @@
+class Processor:
+    def __init__(self):
+        self.pc    = 0
+        self.instructions = []
+        self.data = []
+    
+    def fetch(self, assembly):
+        instruction = assembly[self.pc]
+        self.pc = self.pc + 1
+        return instruction
+    
+    def execute(self, instruction):
+        opcode = instruction[:instruction.find(' ')]
+        operands = instruction.split(' ')[1:]
+        if   (opcode == 'addi'):
+            data[operands[0]] = data[operands[1]] + operands[2]
+        elif (opcode == 'sub'):
+            data[operands[0]] = data[operands[1]] - data[operands[2]]
+        elif (opcode == 'beq'):
+            if (data[operands[0]] == data[operands[1]]):
+                pc = operands[2]
+        elif (opcode == 'ble'):
+            if (data[operands[0]] <= data[operands[1]]):
+                pc = operands[2]
+        elif (opcode == 'j'):
+            pc = operands[0]
+    
+processor = Processor()
+
+print(processor.pc)
