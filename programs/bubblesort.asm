19dbd060e549e7988f9474e320411f03ca6432d5
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-10-19 12:10:26 +0100

    Add lw, sw, and move to bubblesort.asm

--- programs/bubblesort.asm
+++ programs/bubblesort.asm
@@ -14,25 +14,29 @@
 ;   n = new_n
 ; }
 
-.array A: 8, 7, 0, 2, 1, 3, 6, 5, 4, 9  ; int A[10] = {...}
+.A: 8 7 0 2 1 3 6 5 4 9     ; int A[10] = {...}
 
-    li $0 10                            ; int n = 10
-    li $1 1                             ; $1 = 1
+    li $0 10                ; int n = 10
+    li $1 1                 ; $1 = 1
 
 while:
-    blt $0 $1 'while_done'              ; while (n >= 1)
-    li $2 0                             ; int new_n = 0
-    li $3 1                             ; int i = 1
+    blt $0 $1 'while_done'  ; while (n >= 1)
+    li $2 0                 ; int new_n = 0
+    li $3 1                 ; int i = 1
 for:
-    ble A[i-1] A[i] 'less_than'         ; if (A[i-1] > A[i])
-    move $4 A[i-1]                      ; int temp = A[i-1]
-    move A[i-1] A[i]                    ; A[i-1] = A[i]
-    move A[i] $4                        ; A[i] = temp
-    move $2 $3                          ; new_n = i
-less_than:                              ; A[i-1] <= A[i]
-    addi $3 1                           ; i++
-    bge $3 $0 'for_done'                : end for loop if (i >= n)
+    li $4 0                 ; $4 = 0
+    sub $4 $3 $1            ; $4 = i-1
+    lw $5 A($4)             ; $5 = A[i-1]
+    lw $6 A($3)             ; $6 = A[i]
+    ble $5 $6 'less_than'   ; if (A[i-1] > A[i])
+    move $7 $5              ; int temp = A[i-1]
+    sw A($4) $6             ; A[i-1] = A[i]
+    sw A($3) $7             ; A[i] = temp
+    move $2 $3              ; new_n = i
+less_than:                  ; A[i-1] <= A[i]
+    addi $3 1               ; i++
+    blt $3 $0 'for'         ; if (i < n) for loop again
 for_done:
-    move $0 $2                          ; n = new_n
+    move $0 $2              ; n = new_n
 
 while_done:
