f1ca25e87359f7694d9ba3cd1527a1ff51505fa4
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-10-21 21:20:57 +0100

    Add bne, jr, and jal instructions. Update execute to increment PC instead of fetch

--- processor.py
+++ processor.py
@@ -4,13 +4,12 @@ class Processor:
         self.instruction_memory = []
         self.data_memory = []
         self.array_labels = {}
-        self.registers = [0] * 20
+        self.registers = [0] * 30
         self.cycles = 0
         self.instructions_executed = 0
     
     def fetch(self, assembly):
         instruction = assembly.splitlines()[self.pc]
-        self.pc = self.pc + 1
         return instruction
     
     def execute(self, instruction):
@@ -19,36 +18,48 @@ class Processor:
             values = [int(x) for x in instruction.split(' ')[1:]]
             self.array_labels[label] = len(self.data_memory)
             self.data_memory += values
-            return
+            self.pc = self.pc + 1
         opcode = instruction[:instruction.find(' ')]
         operands = instruction.split(' ')[1:]
         if   (opcode == 'addi'):
             self.registers[int(operands[0][1:])] = self.registers[int(operands[1][1:])] + int(operands[2])
-            return
+            self.pc = self.pc + 1
         elif (opcode == 'add'):
             self.registers[int(operands[0][1:])] = self.registers[int(operands[1][1:])] + self.registers[int(operands[2][1:])]
-            return
+            self.pc = self.pc + 1
         elif (opcode == 'sub'):
             self.registers[int(operands[0][1:])] = self.registers[int(operands[1][1:])] - self.registers[int(operands[2][1:])]
-            return
+            self.pc = self.pc + 1
         elif (opcode == 'beq'):
             if (self.registers[int(operands[0][1:])] == self.registers[int(operands[1][1:])]):
                 self.pc = int(operands[2])
-            return
+            else:
+                self.pc = self.pc + 1
+        elif (opcode == 'bne'):
+            if (self.registers[int(operands[0][1:])] != self.registers[int(operands[1][1:])]):
+                self.pc = int(operands[2])
+            else:
+                self.pc = self.pc + 1
         elif (opcode == 'ble'):
             if (self.registers[int(operands[0][1:])] <= self.registers[int(operands[1][1:])]):
                 self.pc = int(operands[2])
-            return
+            else:
+                self.pc = self.pc + 1
         elif (opcode == 'blt'):
             if (self.registers[int(operands[0][1:])] <  self.registers[int(operands[1][1:])]):
                 self.pc = int(operands[2])
-            return
+            else:
+                self.pc = self.pc + 1
         elif (opcode == 'j'):
             self.pc = int(operands[0])
-            return
+        elif (opcode == 'jr'):
+            self.pc = self.registers[int(operands[0][1:])]
+        elif (opcode == 'jal'):
+            self.registers[29] = self.pc + 1
+            self.pc = int(operands[0])
         elif (opcode == 'li'):
             self.registers[int(operands[0][1:])] = int(operands[1])
-            return
+            self.pc = self.pc + 1
         elif (opcode == 'lw'):
             array_op = operands[1]
             label = array_op[:array_op.find('(')]
@@ -59,7 +70,7 @@ class Processor:
                 index = int(index)
             data_index = self.array_labels[label] + index
             self.registers[int(operands[0][1:])] = self.data_memory[data_index]
-            return
+            self.pc = self.pc + 1
         elif (opcode == 'sw'):
             array_op = operands[0]
             label = array_op[:array_op.find('(')]
@@ -70,6 +81,7 @@ class Processor:
                 index = int(index)
             data_index = self.array_labels[label] + index
             self.data_memory[data_index] = self.registers[int(operands[1][1:])]
+            self.pc = self.pc + 1
         elif (opcode == 'move'):
             self.registers[int(operands[0][1:])] = self.registers[int(operands[1][1:])]
-            return
+            self.pc = self.pc + 1
