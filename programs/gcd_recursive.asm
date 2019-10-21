3ec28b96d8942615274e36a12dc2e358451cd6b2
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-10-21 21:21:24 +0100

    Add gcd_recursive.asm

--- programs/gcd_recursive.asm
+++ programs/gcd_recursive.asm
@@ -2,14 +2,31 @@
 ; int b = 39;
 ;
 ; int gcd(int a, int b) {
-;   if (a == b) return a;
-;   if (a > b) return gcd(a-b, b);
+;   if (a == b) {
+;     return a;
+;   }
+;   if (a > b) {
+;     return gcd(a-b, b);
+;   }
 ;   return gcd(a, b-a);
 ; }
 
-    addi r1 r1 27         ; int a = 27
-    addi r2 r2 39         ; int b = 39
+    li $0 27               ; int a = 27
+    li $1 39               ; int b = 39
+    jal 'gcd'              ; call gcd(a, b)
+
+gcd_done:                  ; return value is in $0
+    li $10 1               ; set register 10 to 1
 
 gcd:
-    bne r1 r2 not_equal
-    
+    bne $0 $1 'not_equal'  ; if (a == b)
+    jr $29                 ; jump to return address
+not_equal:
+    ble $0 $1 'less_than'  ; if (a > b)
+    sub $0 $0 $1           ; a = a - b
+    jal 'gcd'              ; call gcd(a - b, b)
+    j 'gcd_done'           ; return
+less_than:
+    sub $1 $1 $0           ; b = b - a
+    jal 'gcd'              ; call gcd(a, b - a)
+    j 'gcd_done'           ; return
