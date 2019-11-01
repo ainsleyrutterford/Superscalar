859176856752fc44e65dda6de836121d8a032ccb
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-11-01 00:41:02 +0000

    Add file input from command line

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ import sys
 
 def main():
 
-    if sys.version_info[0] < 3:
-        print("Python 3 or a more recent version is required.")
-        print("Try: python3 main.py")
+    if (sys.version_info[0] < 3):
+        print('Python 3 or a more recent version is required')
+        print('Try: python3 main.py')
         exit()
 
-    f = open('programs/bubblesort.asm', 'r')
-    assembly = f.read()
+    if (len(sys.argv) != 2):
+        print('Usage: python3 main.py <program.asm>')
+        exit()
+
+    try:
+        file_name = sys.argv[1]
+        f = open(file_name, 'r')
+        assembly = f.read()
+    except IOError:
+        print('File "{0}" does not exist'.format(file_name))
+        exit()
 
     program = assembler.replace_labels(assembler.strip_comments(assembly))
     cpu = processor.Processor()
@@ -26,8 +35,8 @@ def main():
         cpu_history.append(copy.deepcopy(cpu))
     print(cpu.registers)
     print(cpu.data_memory)
-    print("{0} instructions executed in {1} cycles.".format(cpu.instructions_executed, cpu.cycles))
+    print('{0} instructions executed in {1} cycles.'.format(cpu.instructions_executed, cpu.cycles))
     instructions_per_cycle = float(cpu.instructions_executed) / float(cpu.cycles)
-    print("{0:.2f} instructions per cycle achieved.".format(instructions_per_cycle))
+    print('{0:.2f} instructions per cycle achieved.'.format(instructions_per_cycle))
 
 main()
