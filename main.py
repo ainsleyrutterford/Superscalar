94104a80d85fc8ef174e4702089299a478309ef7
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-11-24 01:35:37 +0000

    Clean up main.py

--- main.py
+++ main.py
@@ -2,38 +2,32 @@ import assembler
 import processor
 import sys
 
-def main():
 
-    if (sys.version_info[0] < 3):
-        print('Python 3 or a more recent version is required')
-        print('Try: python3 main.py')
-        exit()
+if (sys.version_info[0] < 3):
+    print('Python 3 or a more recent version is required')
+    print('Try: python3 main.py')
+    exit()
 
-    if (len(sys.argv) != 2):
-        print('Usage: python3 main.py <program.asm>')
-        exit()
+if (len(sys.argv) != 2):
+    print('Usage: python3 main.py <program.asm>')
+    exit()
 
-    try:
-        file_name = sys.argv[1]
-        f = open(file_name, 'r')
-        assembly = f.read()
-    except IOError:
-        print('File "{0}" does not exist'.format(file_name))
-        exit()
+try:
+    file_name = sys.argv[1]
+    f = open(file_name, 'r')
+    assembly = f.read()
+except IOError:
+    print('File "{0}" does not exist'.format(file_name))
+    exit()
 
-    program = assembler.replace_labels(assembler.strip_comments(assembly))
-    cpu = processor.Processor()
+program = assembler.replace_labels(assembler.strip_comments(assembly))
+cpu = processor.Processor()
 
-    while cpu.is_running():
-        cpu.cycle(program)
+while cpu.is_running():
+    cpu.cycle(program)
 
-    # for i in range(100):
-    #     cpu.cycle(program)
-
-    print('Registers: {0}'.format(cpu.registers))
-    print('Memory: {0}'.format(cpu.data_memory))
-    print('{0} instructions executed in {1} cycles.'.format(cpu.instructions_executed, cpu.cycles))
-    instructions_per_cycle = float(cpu.instructions_executed) / float(cpu.cycles)
-    print('{0:.2f} instructions per cycle achieved.'.format(instructions_per_cycle))
-
-main()
+print(f'Registers: {cpu.registers}')
+print(f'Memory: {cpu.data_memory}')
+print(f'{cpu.instructions_executed} instructions executed in {cpu.cycles} cycles.')
+instructions_per_cycle = float(cpu.instructions_executed) / float(cpu.cycles)
+print(f'{instructions_per_cycle:.2f} instructions per cycle achieved.')
