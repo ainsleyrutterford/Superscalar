58a5689d91c0619cb13dc2c66a6b31cae252a4bf
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-11-28 21:50:18 +0000

    Remove pipelining to start afresh

--- processor.py
+++ processor.py
@@ -1,11 +1,3 @@
-class Pipe:
-    def __init__(self, pc):
-        self.pc = pc
-        self.instruction = None
-        self.opcode = None
-        self.operand = None
-        self.stage = 'Fetch'
-
 class Processor:
     def __init__(self):
         self.pc = 0
@@ -15,37 +7,12 @@ class Processor:
         self.registers = [0] * 32
         self.cycles = 0
         self.instructions_executed = 0
-        self.pipeline_width = 3
-        self.pipeline = [None] * self.pipeline_width
-
-    def fill_next_pipe(self):
-        if (None in self.pipeline):
-            next_none = self.pipeline.index(None)
-            self.pipeline[next_none] = Pipe(self.pc)
-
-    def flush_pipeline(self):
-        self.pipeline = [None] * self.pipeline_width
-    
-    def execute_pipes(self, assembly):
-        for i, pipe in enumerate(self.pipeline):
-            if pipe != None:
-                if pipe.stage == 'Fetch':
-                    pipe.instruction = self.fetch(assembly)
-                    pipe.stage = 'Decode'
-                elif pipe.stage == 'Decode':
-                    (pipe.opcode, pipe.operand) = self.decode(pipe.instruction)
-                    pipe.stage = 'Execute'
-                elif pipe.stage == 'Execute':
-                    branch = self.execute(pipe.opcode, pipe.operand, pipe.pc)
-                    self.pipeline[i] = None
-                    if branch:
-                        self.flush_pipeline()
-                        return
 
     def cycle(self, assembly):
-        self.fill_next_pipe()
-        self.execute_pipes(assembly)
-        self.cycles += 1
+        instruction = self.fetch(assembly)
+        opcode, operands = self.decode(instruction)
+        self.execute(opcode, operands, self.pc)
+        self.cycles += 3
 
     def fetch(self, assembly):
         if (self.pc >= len(assembly.splitlines())):
@@ -68,7 +35,7 @@ class Processor:
             operands = instruction.split(' ')[1:]
         return (opcode, operands)
     
-    def execute(self, opcode, operands, pipe_pc):
+    def execute(self, opcode, operands, current_pc):
         if   (opcode == 'addi'):
             self.registers[int(operands[0][1:])] = self.registers[int(operands[1][1:])] + int(operands[2])
         elif (opcode == 'add'):
@@ -78,29 +45,22 @@ class Processor:
         elif (opcode == 'beq'):
             if (self.registers[int(operands[0][1:])] == self.registers[int(operands[1][1:])]):
                 self.pc = int(operands[2])
-                return True
         elif (opcode == 'bne'):
             if (self.registers[int(operands[0][1:])] != self.registers[int(operands[1][1:])]):
                 self.pc = int(operands[2])
-                return True
         elif (opcode == 'ble'):
             if (self.registers[int(operands[0][1:])] <= self.registers[int(operands[1][1:])]):
                 self.pc = int(operands[2])
-                return True
         elif (opcode == 'blt'):
             if (self.registers[int(operands[0][1:])] <  self.registers[int(operands[1][1:])]):
                 self.pc = int(operands[2])
-                return True
         elif (opcode == 'j'):
             self.pc = int(operands[0])
-            return True
         elif (opcode == 'jr'):
             self.pc = self.registers[int(operands[0][1:])]
-            return True
         elif (opcode == 'jal'):
-            self.registers[29] = pipe_pc + 1
+            self.registers[29] = current_pc
             self.pc = int(operands[0])
-            return True
         elif (opcode == 'li'):
             self.registers[int(operands[0][1:])] = int(operands[1])
         elif (opcode == 'lw'):
@@ -126,7 +86,6 @@ class Processor:
         elif (opcode == 'move'):
             self.registers[int(operands[0][1:])] = self.registers[int(operands[1][1:])]
         self.instructions_executed += 1
-        return False
     
     def is_running(self):
         if self.registers[31] == 0:
