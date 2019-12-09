b59ed1b52d0836962e069504ec92bec2c0036129
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-12-09 16:39:52 +0000

    Clean up main.py

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ cpu = processor.Processor()
 # while cpu.is_running():
 #     cpu.cycle(program)
 
-# for i in range(len(program.splitlines())):
 while(1):
     input()
     print('Cycling cpu...')
