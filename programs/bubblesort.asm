c842e2c719bfed2dc4d6e8be0bab19f466342541
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-12-10 12:30:45 +0000

    Fix bubblesort.asm

--- programs/bubblesort.asm
+++ programs/bubblesort.asm
@@ -16,13 +16,13 @@
 
 .A: 8 7 0 2 1 3 6 5 4 9     ; int A[10] = {...}
 
-    addi $0 $0 10           ; int n = 10
+    addi $10 $10 10         ; int n = 10
     addi $1 $1 1            ; $1 = 1
 
 while:
-    blt $0 $1 'while_done'  ; while (n >= 1)
-    addi $2 $2 0            ; int new_n = 0
-    addi $3 $3 1            ; int i = 1
+    blt $10 $1 'while_done' ; while (n >= 1)
+    add $2 $0 $0            ; int new_n = 0
+    add $3 $1 $0            ; int i = 1
 for:
     sub $4 $3 $1            ; $4 = i-1
     lw $5 A($4)             ; $5 = A[i-1]
@@ -34,9 +34,9 @@ for:
     addi $2 $3 0            ; new_n = i
 less_than:                  ; A[i-1] <= A[i]
     addi $3 $3 1            ; i++
-    blt $3 $0 'for'         ; if (i < n) for loop again
+    blt $3 $10 'for'        ; if (i < n) for loop again
 for_done:
-    addi $0 $2 0            ; n = new_n
+    addi $10 $2 0           ; n = new_n
     j 'while'
 
 while_done:
